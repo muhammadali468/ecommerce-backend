d7ca57620b54e97dc0155d752caf0cc6206ff7d9
muhammadali468 <muhammad.ali.dev.in@gmail.com>
2026-02-08 14:32:11 +0500

    db fix

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ require("dotenv").config()
 const mongoose = require("mongoose");
 const DB_URL = process.env.DB_URL
 
-mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(DB_URL);
 
 mongoose.connection.on("connected",()=>{
     console.log("Connected to MongoDB");
