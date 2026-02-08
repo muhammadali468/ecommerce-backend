81423a3f40226d1ce64e4e15f60e8b09cc8a05f6
muhammadali468 <muhammad.ali.dev.in@gmail.com>
2026-02-08 16:45:05 +0500

    cloud img uploading

--- routes/Admin/addCategory.js
+++ routes/Admin/addCategory.js
@@ -2,11 +2,17 @@ const express = require("express");
 const multer = require("multer");
 const shortid = require("shortid");
 const path = require("path");
+const cloudinary = require("cloudinary").v2
 // const AddCategorySchema = require("../../models/AddCategory");
 const AddCategorySchema = require("../../models/AddCategory");
 
 const router = express.Router();
 
+cloudinary.config({
+    cloud_name: "donm326ji",
+    api_key: "111481265588618",
+    api_secret: "5oLyoclV17XIR6nP2FMK_3QKbB0"
+})
 
 
 // image storage engine setup
@@ -26,14 +32,17 @@ const uploadCat = multer({
 })
 
 // http://localhost:5000/api/category/add
-router.post("/category/add", uploadCat.single("cat_img"), async (req, res) => {
+router.post("/category/add", async (req, res) => {
     const cat_name = req.body.cat_name;
     const cat_file = req.file.filename;
+    const cloudImg = await cloudinary.uploader.upload(cat_file, {
+        folder:"categories"
+    })
     const newCat = new AddCategorySchema({
         cat_name,
-        cat_img:cat_file
+        cat_img:cloudImg.secure_url
     })
-
+    console.log(newCat)
     newCat.save()
     return res.json({sts:0,msg:"Category Added!"})
 })
--- routes/Product/addProduct.js
+++ routes/Product/addProduct.js
@@ -4,6 +4,8 @@ const shortid = require("shortid");
 const path = require("path");
 const AddProductSchema = require("../../models/AddProduct");
 const AddProductImg = require("../../models/AddProductImg");
+const cloudinary = require("cloudinary").v2
+
 
 const router = express.Router();
 
@@ -23,31 +25,37 @@ const uploadPro = multer({
     limits: { fileSize: 1024 * 1024 * 5 }
 })
 
-const uploadImages = multer({storage:proStore});
+const uploadImages = multer({ storage: proStore });
+
+cloudinary.config({
+    cloud_name: "donm326ji",
+    api_key: "111481265588618",
+    api_secret: "5oLyoclV17XIR6nP2FMK_3QKbB0"
+})
 
 // http://localhost:5000/api/product/uploadimages/id
-router.post("/product/uploadimages/:id", uploadImages.array("images"),async(req,res)=>{
+router.post("/product/uploadimages/:id", uploadImages.array("images"), async (req, res) => {
     const productId = req.params.id;
     const imageFiles = req.files;
 
     try {
-        const imagePromises = imageFiles.map((file)=>{
+        const imagePromises = imageFiles.map((file) => {
             const newProImages = new AddProductImg({
                 productId,
-                productImg:file.filename
+                productImg: file.filename
             })
             return newProImages.save()
         })
         await Promise.all(imagePromises)
-        res.json({sts:0,msg:"Images uploaded!"})
-        
+        res.json({ sts: 0, msg: "Images uploaded!" })
+
     } catch (error) {
         console.log(error)
     }
 })
 
 // http://localhost:5000/api/product/add
-router.post("/product/add", uploadPro.single("productThumbnailImg"), async (req, res) => {
+router.post("/product/add", async (req, res) => {
     const {
         productCategory,
         productName,
@@ -60,6 +68,9 @@ router.post("/product/add", uploadPro.single("productThumbnailImg"), async (req,
     } = req.body;
     const productThumbnailImg = req.file.filename;
     try {
+        const uploadResult = await cloudinary.uploader.upload(productThumbnailImg,{
+            folder:"products"
+        })
         const newProduct = new AddProductSchema({
             productCategory,
             productName,
@@ -67,7 +78,7 @@ router.post("/product/add", uploadPro.single("productThumbnailImg"), async (req,
             productLongDescription,
             productPrice,
             productSalePrice,
-            productThumbnailImg,
+            productThumbnailImg:uploadResult.secure_url,
             productSaleStartDate,
             productSaleEndDate,
         })
