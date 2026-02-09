7fb363f2cb2d2552f7f67eb2f17c948c1f215342
muhammadali468 <muhammad.ali.dev.in@gmail.com>
2026-02-09 19:31:11 +0500

    exception handling of get products when category is deleted

--- routes/Product/getProducts.js
+++ routes/Product/getProducts.js
@@ -2,12 +2,10 @@ const express = require("express");
 const AddProductSchema = require("../../models/AddProduct");
 const router = express.Router();
 
-
 // http://localhost:5000/api/products/get
 router.get("/products/get", async (req, res) => {
     try {
         const allProducts = await AddProductSchema.find().populate("productCategory");
-        // console.log(allProducts[allProducts.length - 1].productCategory)
         if (allProducts) {
             const formatedProducts = allProducts.map((product) => {
                 return {
@@ -21,7 +19,7 @@ router.get("/products/get", async (req, res) => {
                     productThumbnailImg:product.productThumbnailImg,
                     productSaleStartDate:product.SaleStartDate,
                     productSaleEndDate:product.productSaleEndDate,
-                    productCategoryName:product.productCategory.cat_name,
+                    productCategoryName:product.productCategory?.cat_name || "uncategorized",
                     productStatus:product.productStatus
                 }
             })
